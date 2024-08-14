de7d2d38ba120498bc9f8acbddebcaff1c51b242
Veksoe <Anniika-xb@live.dk>
2024-08-14 12:12:24 +0200

    Index and word scramble game
    - Basic design for index
    - Basic design for word scramble game
    - Functionality for word scramble game

---
+++ assets/javascript/wordScramble.js
@@ -0,0 +1,130 @@
+const wordEl = document.querySelector(".word"),
+    hintEl = document.querySelector(".hint"),
+    checkBtnEl = document.querySelector(".checkWord"),
+    userWordEl = document.querySelector(".wordInput"),
+    streakEl = document.querySelector(".streak span"),
+    messageEl = document.querySelector(".message"),
+    timeEl = document.querySelector(".counter"),
+    restartBtnEl = document.querySelector(".restart");
+
+let correctWord, timer;
+let currentStreak = 0;
+let tempWords = [];
+
+function initTimer(maxTime) {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            return timeEl.textContent = maxTime
+        }
+        clearInterval(timer);
+        messageEl.classList.remove("wrong")
+        messageEl.classList.remove("correct")
+        messageEl.textContent = `Time is up! "${correctWord.toLocaleUpperCase()}" was the correct word. Your streak of ${currentStreak} have reset.`
+        currentStreak = 0;
+        initGame()
+    }, 1000)
+
+}
+
+function initGame() {
+    initTimer(30);
+    userWordEl.focus()
+    let randomnNumber = Math.floor(Math.random() * words.length)
+    let randomObj = words[randomnNumber];
+    tempWords.push(randomObj)
+    let wordArray = randomObj.word.split("");
+    streakEl.textContent = currentStreak;
+
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = wordArray[i]; //stores the element at i temporarily
+        wordArray[i] = wordArray[j]; //assigns the element at index 'j' to index 'i'
+        wordArray[j] = temp // assigns the temporarily stored element to index 'j'.
+    }
+    wordEl.textContent = wordArray.join("");
+    hintEl.textContent = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    userWordEl.value = ""
+    userWordEl.setAttribute("maxlength", correctWord.length)
+
+    words.splice(randomnNumber, 1)
+
+}
+
+function checkWord() {
+    let userInput = userWordEl.value.toLocaleLowerCase();
+    if (!userInput && words.length > 0) {
+        messageEl.classList.remove("wrong")
+        messageEl.classList.remove("correct")
+        messageEl.textContent = `Please enter a word`;
+        return;
+    }
+    if (userInput !== correctWord && words.length > 0) {
+        messageEl.classList.remove("correct")
+        messageEl.classList.add("wrong")
+        messageEl.textContent = `Oops! "${userInput.toLocaleUpperCase()}" was not the correct word, it was "${correctWord.toLocaleUpperCase()}". Your streak of ${currentStreak} have reset.`
+        currentStreak = 0;
+        initGame()
+        return;
+    }
+
+    if (words.length === 0 && userInput === correctWord) {
+        currentStreak++
+        streakEl.textContent = currentStreak;
+        messageEl.classList.remove("wrong")
+        messageEl.classList.add("correct")
+        clearInterval(timer);
+        words.push.apply(words, tempWords);
+        tempWords = [];
+        console.log("words: ", words)
+        console.log("Temp words: ", tempWords)
+        checkBtnEl.classList.add("hidden")
+        restartBtnEl.classList.remove("hidden")
+        userWordEl.setAttribute("disabled", "disabled")
+        return messageEl.textContent = `Congratulation! "${userInput.toLocaleUpperCase()}" was the correct word. Glittergumber doesn't have any more words on his list.`
+    }
+    if (words.length === 0 && userInput !== correctWord) {
+        currentStreak = 0;
+        streakEl.textContent = currentStreak;
+        messageEl.classList.remove("correct")
+        messageEl.classList.add("wrong")
+        clearInterval(timer);
+        words.push.apply(words, tempWords);
+        tempWords = [];
+        console.log("words: ", words)
+        console.log("Temp words: ", tempWords)
+        checkBtnEl.classList.add("hidden")
+        restartBtnEl.classList.remove("hidden")
+        userWordEl.setAttribute("disabled", "disabled")
+        return messageEl.textContent = `Oops! "${userInput.toLocaleUpperCase()}" was not the correct word, it was "${correctWord.toLocaleUpperCase()}". Glittergumber doesn't have any more words on his list.`
+    }
+
+    currentStreak++
+    messageEl.classList.remove("wrong")
+    messageEl.classList.add("correct")
+    messageEl.textContent = `Congatulation! "${userInput.toLocaleUpperCase()}" was the correct word!`
+    initGame();
+
+}
+
+
+
+initGame();
+
+if (userWordEl) {
+    userWordEl.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            checkWord()
+        }
+    })
+}
+if (checkBtnEl) {
+    checkBtnEl.addEventListener("click", checkWord)
+}
+if (restartBtnEl) {
+    restartBtnEl.addEventListener("click", () => {
+        location.reload()
+    })
+}
---
+++ assets/javascript/words.js
@@ -0,0 +1,77 @@
+const words = [
+    {
+        word: "trowel",
+        hint: "A small hand tool used for digging and planting in the garden."
+    },
+
+    {
+        word: "hoe",
+        hint: "A tool used for cultivating the soil and removing weeds."
+    },
+    {
+        word: "spade",
+        hint: "A flat-bladed tool used for digging and edging in the garden."
+    },
+    {
+        word: "rake",
+        hint: "A tool with a long handle and many teeth used to gather leaves and debris."
+    },
+    {
+        word: "wheelbarrow",
+        hint: "A one- or two-wheeled cart used for transporting soil, plants, and tools around the garden."
+    },
+    {
+        word: "mulch",
+        hint: "Material spread over soil to retain moisture, suppress weeds, and improve soil quality."
+    },
+    {
+        word: "shears",
+        hint: "Handheld tool used for cutting and shaping plants, especially shrubs and hedges."
+    },
+    {
+        word: "compost",
+        hint: "Organic material that is decomposed and used to enrich garden soil."
+    },
+    {
+        word: "sunflower",
+        hint: "Tracks the sun's movement"
+    },
+    {
+        word: "tulip",
+        hint: "Named after a Turkish word for 'turban'"
+    },
+    {
+        word: "lavender",
+        hint: "Used in perfumery and ancient rituals, believed to ward off evil spirits."
+    },
+    {
+        word: "sunlight",
+        hint: "Vital energy source that plants convert into food through photosynthesis."
+    },
+    {
+        word: "pollinator",
+        hint: "Agent, such as bees or butterflies, that transfers pollen from flower to flower."
+    },
+    {
+        word: "butterfly",
+        hint: "A colorful insect with large, often patterned wings, known for its metamorphosis from a larval caterpillar stage and its role in pollination."
+    },
+    {
+        word: "ladybug",
+        hint: "Beneficial beetle often red with black spots, known for consuming aphids and other garden pests."
+    },
+    {
+        word: "grasshopper",
+        hint: "Jumping insect with long hind legs, known for its role in agriculture and occasional crop damage."
+    },
+    {
+        word: "catarpillar",
+        hint: "The larval stage of butterflies and moths, often voracious and a key food source for many birds."
+    },
+    {
+        word: "dragonfly",
+        hint: "Predatory insect with iridescent wings, known for its agile flight and predation on other insects."
+    },
+
+
+]
